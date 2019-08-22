0cefa6ed42d1a3a51f49fdccede0ae9e271e6bc4
Евгений Красильников <Kras076.99@outlook.com>
2019-08-22 21:48:21 +0300

    Create PizzaMigration

---
+++ PizzaShop/Migrations/20190822164136_InitialMigration.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaShop.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diameters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    DiameterValue = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diameters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pizzas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Name = table.Column<string>(type: "varchar(200)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizzas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PizzaDiameters",
+                columns: table => new
+                {
+                    DiameterId = table.Column<int>(nullable: false),
+                    PizzaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PizzaDiameters", x => new { x.DiameterId, x.PizzaId });
+                    table.ForeignKey(
+                        name: "FK_PizzaDiameters_Diameters_DiameterId",
+                        column: x => x.DiameterId,
+                        principalTable: "Diameters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PizzaDiameters_Pizzas_PizzaId",
+                        column: x => x.PizzaId,
+                        principalTable: "Pizzas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PizzaDiameters_PizzaId",
+                table: "PizzaDiameters",
+                column: "PizzaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PizzaDiameters");
+
+            migrationBuilder.DropTable(
+                name: "Diameters");
+
+            migrationBuilder.DropTable(
+                name: "Pizzas");
+        }
+    }
+}
