f2111f5e6da7aef9cd725da5e766b792ad1934e3
Евгений Красильников <Kras076.99@outlook.com>
2019-08-22 21:51:55 +0300

    Add Price To Pizza Diameter Migration

---
+++ PizzaShop/Migrations/20190822164455_AddPriceToPizzaDiametersTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaShop.Migrations
+{
+    public partial class AddPriceToPizzaDiametersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "PizzaDiameters",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "PizzaDiameters");
+        }
+    }
+}
