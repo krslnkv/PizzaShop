cee2d09bbbc28f57fc9e60a55a1ce587b66e5806
Евгений Красильников <Kras076.99@outlook.com>
2019-08-22 21:53:12 +0300

    Create User and Role migrations

---
+++ PizzaShop/Migrations/20190822171507_AddUsersAndRolesTables2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaShop.Migrations
+{
+    public partial class AddUsersAndRolesTables2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
