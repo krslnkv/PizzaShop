c8827e6b0931d42fa3b381ca7b32a43b6e4e03c0
Евгений Красильников <Kras076.99@outlook.com>
2019-08-22 21:51:25 +0300

    Create Dameter and PizzaDiameter Models

---
+++ PizzaShop/Models/PizzaDiameter.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MySql.Data.EntityFrameworkCore.DataAnnotations;
+
+namespace PizzaShop.Models
+{
+    [MySqlCharset("utf8")]
+    public class PizzaDiameter
+    {
+        [Key]
+        public int Id { get; set; }
+        public int DiameterId { get; set; }
+        public Diameter Diameter { get; set; }
+        public int PizzaId { get; set; }
+        public Pizza Pizza { get; set; }
+        public int Price { get; set; }
+    }
+}
