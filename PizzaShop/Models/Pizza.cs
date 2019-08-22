f71d085fdfd92b4d030d1487824fc11570133d01
Евгений Красильников <Kras076.99@outlook.com>
2019-08-22 21:47:57 +0300

    Create Pizza Model

---
+++ PizzaShop/Models/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MySql.Data.EntityFrameworkCore.DataAnnotations;
+
+namespace PizzaShop.Models
+{
+    [MySqlCharset("utf8")]
+    public class Pizza
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
