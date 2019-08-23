e0af830cfb491a3e9e0e7f1d1e65f7a3b3b96050
Евгений Красильников <Kras076.99@outlook.com>
2019-08-23 19:43:36 +0300

    Add VueJs PizzaListComponent

---
+++ PizzaShop/wwwroot/js/VuePizzaListComponent.js
@@ -0,0 +1,27 @@
+﻿let pizzaListComponent = new Vue(
+    {
+        el: '#pizza-list-component',
+        data: {
+            pizzas:[]
+            
+        },
+        methods: {
+            loadData: function () {
+                let self = this;
+                
+                axios.get('/Api/GetPizzas')
+                    .then(function (response) {
+                        self.pizzas=response.data;
+                        console.log(self.pizzas);
+                    })
+                    .catch(function (error) {
+                        alert("ERROR: " + (error.message|error));
+                    });
+                
+            }
+        },
+        created: function () {
+            this.loadData();
+        }
+    }
+)
