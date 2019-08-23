6c9bb7a5282fedbcb85cf520a1958f11ec8a695e
Евгений Красильников <Kras076.99@outlook.com>
2019-08-23 19:42:10 +0300

    Create ApiController

---
+++ PizzaShop/Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PizzaShop.Models;
+using PizzaShop.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaShop.Controllers
+{
+    public class ApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetPizzas()
+        {
+            var psContext = new PizzaShopContext();
+            var pizzas = await psContext.PizzaDiameters.Include(pd => pd.Pizza).Include(pd => pd.Diameter)
+                .ToListAsync();
+            if (pizzas.Count == 0)
+                return new NotFoundResult();
+            else 
+                return new OkObjectResult(pizzas);
+
+        }
+    }
+}
